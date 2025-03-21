b43d110e2a6c8e67720f56dbdeb5c78ffe3626b9
kyumin1227 <kyumin12271227@gmail.com>
2025-03-21 23:34:53 +0900

    Lesson: 경사 하강법 (BGD)

---
+++ chp_01/bgd.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Data set
+# input
+# input data, features
+# H(x) -> input data : x1 -> xn
+x_train = [ np.random.rand() * 10 for _ in range(50)]
+y_train = [val + np.random.rand() * 5 for val in x_train]
+
+
+# BGD (Batch Gradient Descent) 배치경사하강법을 
+# 이용하여 Linear Rergeresion 적용
+w = 0.0
+b = 0.0
+learning_rate = 0.01
+epoch = 100
+loss_list = []
+
+# H(x) -> w * x + b
+for num_of_epoch in range(epoch):
+    loss = 0
+    gradient_w_sum = 0.0
+    gradient_b_sum = 0.0
+    # GD 수행 후 최적의 W 값 도출
+    for x, y in zip(x_train, y_train):
+        gradient_w_sum += x * (w * x + b - y)
+        gradient_b_sum += (w * x + b - y)
+        loss += pow((w * x - y), 2) 
+    # W 값 업데이트
+    w = w - learning_rate * (gradient_w_sum / len(x_train))
+    b = b - learning_rate * (gradient_b_sum / len(x_train))
+    loss_list.append(loss)
+    
+x_test = [val for val in range(10)]
+y_test = [w * val + b for val in x_test]
+
+plt.scatter(x_train,y_train, color ="blue")
+plt.plot(x_test, y_test)
+plt.show()
+
+# 손실률
+plt.plot(list(range(epoch)), loss_list)
+plt.show()
+
+# output
+# label
+# f(x1) -> f(x2)
